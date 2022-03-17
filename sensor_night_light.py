4452e0c7d0a25965f12415f8d96b2ea895acc8fe
worksonmymachine <jazzfreund@gmx.de>
2022-03-17 14:22:14 +0100

    Create sensor_night_light.py

---
+++ sensor_night_light.py
@@ -0,0 +1,82 @@
+from machine import Pin
+import machine
+from time import sleep
+
+class Sensor:
+    def __init__(self, pin, pull_down=True):
+        self._sensor = Pin(pin, Pin.IN, Pin.PULL_DOWN if pull_down else Pin.PULL_UP)
+
+    def isTriggered(self):
+        return self._sensor.value() == 1
+    
+    def isNotTriggered(self):
+        return not self.isTriggered()
+    
+    
+class Led:
+    def __init__(self, pin, init_state_off=True, blink_delay=0.5, smart_blink=True):
+        self._led = Pin(pin, Pin.OUT)
+        self._blink_delay = blink_delay
+        self._smart_blink = smart_blink
+        self._led.off() if init_state_off else self._led.on()
+
+    def on(self):
+        if self.isOff():
+            self._led.high()
+
+    def off(self):
+        if self.isOn():
+            self._led.low()
+
+    def isOn(self):
+        return self._led.value() == 1
+    
+    def isOff(self):
+        return not self.isOn()
+    
+    def toggle(self):
+        self._led.off() if self.isOn() else self.on()
+        
+    def blink(self):
+        self._smart_blink() if self._smart_blink else self._blink()
+        
+    def _blink(self):
+        self.on()
+        self._blink_delay()
+        self.off()
+        
+    def _smart_blink(self):
+        self.toggle()
+        self._blink_delay()
+        self.toggle()
+        
+    def _blinkDelay(self):
+        sleep(self._blink_delay)
+
+
+class SensorNightLight:
+
+    def __init__(self, trigger_interval=1, trigger_duration=30):
+        self.led = Led(4)
+        self.light = Sensor(27)
+        self.motion = Sensor(28)
+        self.trigger_interval = trigger_interval
+        self.trigger_duration = trigger_duration
+        self._loop()
+
+    def _loop(self):
+        while True:
+            print("light sensor TRIGGERED") if self.light.isTriggered() else print("light sensor SLEEPING")
+            print("motion sensor TRIGGERED") if self.motion.isTriggered() else print("motion sensor SLEEPING")
+            if self.light.isTriggered():
+                print("dark enough - waiting for motion")
+                while self.motion.isTriggered():
+                    print(f"motion sensor triggered - LED on and checking again in {self.trigger_duration}")
+                    self.led.on()
+                    sleep(self.trigger_duration)  
+            self.led.off()
+            sleep(self.trigger_interval)
+    
+
+if __name__ == "__main__":
+    SensorNightLight()
