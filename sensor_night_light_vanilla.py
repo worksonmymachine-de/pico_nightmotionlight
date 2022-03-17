6303d3c02faf9d7b178dfe3569d8a0e3ee6bc01c
worksonmymachine <jazzfreund@gmx.de>
2022-03-17 16:24:40 +0100

    more default values

--- sensor_night_light_vanilla.py
+++ sensor_night_light_vanilla.py
@@ -4,10 +4,10 @@ from time import sleep
 
 class SensorNightLight:
 
-    def __init__(self, trigger_interval=1, trigger_duration=30):
-        self.led = Pin(4, Pin.OUT)
-        self.light = Pin(27, Pin.IN, Pin.PULL_DOWN)
-        self.motion = Pin(28, Pin.IN, Pin.PULL_DOWN)
+    def __init__(self, led_pin=4, light_pin=27, motion_pin=28, trigger_interval=1, trigger_duration=30):
+        self.led = Pin(led_pin, Pin.OUT)
+        self.light = Pin(light_pin, Pin.IN, Pin.PULL_DOWN)
+        self.motion = Pin(motion_pin, Pin.IN, Pin.PULL_DOWN)
         self.trigger_interval = trigger_interval
         self.trigger_duration = trigger_duration
         self._loop()
